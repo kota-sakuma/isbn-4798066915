68f8d5e86a5b0910522b9ba04c58c96c4ec19f85
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-03 10:18:46 +0900

    feat: add hello router to app.js

--- express_app/app.js
+++ express_app/app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import cookieParser from 'cookie-parser';
+import createError from 'http-errors';
+import express from 'express';
+import logger from 'morgan';
+import path from 'path';
+import helloRouter from './routes/hello';
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,14 +21,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
