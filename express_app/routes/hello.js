43cd7b523f557f6c2088efa1c2137792ba877733
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-03 10:38:31 +0900

    feat: add new variables to hello.js

--- express_app/routes/hello.js
+++ express_app/routes/hello.js
@@ -2,8 +2,20 @@ import { Router } from 'express';
 
 const router = Router();
 
+const db = [
+  { name: 'taro', mail: 'taro@yamada' },
+  { name: 'hanako', mail: 'hanako@flower' },
+  { name: 'sachiko', mail: 'sachiko@happy' },
+  { name: 'jiro', mail: 'jiro@change' },
+]
+
 router.get('/', (req, res, next) => {
-  res.render('hello', { title: 'Hello!', message: 'これは、サンプルで追加したページです。' });
+  const data = {
+    title: 'Hello!',
+    message: 'これは、サンプルで追加したページです。',
+    db: db,
+  }
+  res.render('hello', data);
 });
 
 export default router;
