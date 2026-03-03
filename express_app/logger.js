99ae769ad76b39867e2a9aeeafb4dc2daacfd5da
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-03 14:06:20 +0900

    feat: add logger.js

---
+++ express_app/logger.js
@@ -0,0 +1,18 @@
+import pino from 'pino';
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+export const logger = pino({
+  level: process.env.LOG_LEVEL || (isDev ? 'debug' : 'info'),
+  ...(isDev && {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'SYS:standard',
+      },
+    },
+  }),
+});
+
+export default logger;
