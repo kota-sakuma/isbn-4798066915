433951c73bf50be31bdea0e0b20f2a6b6e05a306
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-03-04 15:43:04 +0900

    feat: modify the get router of db.js

--- express_app/routes/db.js
+++ express_app/routes/db.js
@@ -7,7 +7,19 @@ const router = Router();
 
 router.get('/', async (req, res, next) => {
   const users = await prisma.user.findMany();
-  res.json(users);
+  logger.info({}, 'users fetched');
+  logger.debug({ users: users }, 'users');
+
+  const data = {
+    title: 'Prisma',
+    message: 'List of the Users table',
+    data: users,
+  };
+  logger.info({}, 'data created');
+  logger.debug({ data: data }, 'data');
+
+  res.render('db', data);
+  logger.info({}, 'render completed');
 });
 
 export default router;
