1129b9de8e48e02fa93f99599b3728c26de09ab7
kota-sakuma <kotasakuma98@gmail.com>
2026-03-05 21:11:16 +0900

    feat: implement prisma form

--- express_app/routes/db.js
+++ express_app/routes/db.js
@@ -1,27 +1,39 @@
-import logger from '../logger.js';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '../../generated/prisma/client.ts';
-import { Router } from 'express';
+import logger from "../logger.js";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../../generated/prisma/client.ts";
+import { Router } from "express";
 
 const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 const router = Router();
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   const users = await prisma.user.findMany();
-  logger.info({}, 'users fetched');
-  logger.debug({ users: users }, 'users');
+  logger.info({}, "users fetched");
+  logger.debug({ users: users }, "users");
 
   const data = {
-    title: 'Prisma',
-    message: 'List of the Users table',
+    title: "Prisma",
+    message: "List of the Users table",
     data: users,
   };
-  logger.info({}, 'data created');
-  logger.debug({ data: data }, 'data');
+  logger.info({}, "data created");
+  logger.debug({ data: data }, "data");
 
-  res.render('db', data);
-  logger.info({}, 'render completed');
+  res.render("db", data);
+  logger.info({}, "render completed");
+});
+
+router.post("/", async (req, res, next) => {
+  const { name, email, age } = req.body;
+  const user = await prisma.user.create({
+    data: { name: name, email: email, age: age },
+  });
+  logger.info({}, "user created");
+  logger.debug({ user: user }, "user");
+
+  res.redirect("/db");
+  logger.info({}, "redirect completed");
 });
 
 export default router;
